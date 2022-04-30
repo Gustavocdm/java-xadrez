4d8492cffa99a49a5c6cb85c250f1328e0fc7e76
thiago natividade <thiagonatividadepereira02@gmail.com>
2022-04-30 16:12:25 -0300

    pacote utils e classe de posicao da peça

---
+++ src/main/java/br/com/gustavomelo/xadrez/utils/Posicao.java
@@ -0,0 +1,14 @@
+package br.com.gustavomelo.xadrez.utils;
+
+public class Posicao {
+	private int linha;
+	private int coluna;
+	
+	public Posicao(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+
+
+	
+}
