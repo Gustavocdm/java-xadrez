30cec97dc5ff92af3e7d9226aed26f66ddd8b83d
Gustavo <47989383+Gustavocdm@users.noreply.github.com>
2022-05-02 10:30:52 -0300

    adotado um padrão de nomeclatura dos métodos dos testes unitários

--- src/test/java/br/com/gustavomelo/xadrez/utils/PosicaoTest.java
+++ src/test/java/br/com/gustavomelo/xadrez/utils/PosicaoTest.java
@@ -5,7 +5,6 @@ import static br.com.gustavomelo.xadrez.utils.Posicao.POSICAO_MINIMA_PERMITIDA;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import br.com.gustavomelo.xadrez.exceptions.PosicaoInvalidaException;
@@ -16,40 +15,35 @@ public class PosicaoTest {
 	private static final int POSICAO_VALIDA = 1;
 	
 	@Test
-	@DisplayName("Deve lançar exceção se linha passada no construtor for menor que o mínimo permitido")
-	public void linhaMenorInvalidaExceptionTest() throws PosicaoInvalidaException {
+	public void construtor_deveLancarPosicaoInvalidaException_seLinhaPassadaForMenorQueOPermitido() throws PosicaoInvalidaException {
 		assertThrows(PosicaoInvalidaException.class, () -> {
 			new Posicao(POSICAO_ABAIXO_DO_MINIMO, POSICAO_VALIDA);
 		});
 	}
 	
 	@Test
-	@DisplayName("Deve lançar exceção se linha passada no construtor for maior que o máximo permitido")
-	public void linhaMaiorInvalidaExceptionTest() throws PosicaoInvalidaException {
+	public void construtor_deveLancarPosicaoInvalidaException_seLinhaPassadaForMaiorQueOPermitido() throws PosicaoInvalidaException {
 		assertThrows(PosicaoInvalidaException.class, () -> {
 			new Posicao(POSICAO_VALIDA, POSICAO_ACIMA_DO_MAXIMO);
 		});
 	}
 	
 	@Test
-	@DisplayName("Deve lançar exceção se coluna passada no construtor for menor que o mínimo permitido")
-	public void colunaMenorInvalidaExceptionTest() throws PosicaoInvalidaException {
+	public void construtor_deveLancarPosicaoInvalidaException_seColunaPassadaForMenorQueOPermitido() throws PosicaoInvalidaException {
 		assertThrows(PosicaoInvalidaException.class, () -> {
 			new Posicao(POSICAO_ABAIXO_DO_MINIMO, POSICAO_VALIDA);
 		});
 	}
 	
 	@Test
-	@DisplayName("Deve lançar exceção se coluna passada no construtor for maior que o máximo permitido")
-	public void colunaMaiorInvalidaExceptionTest() throws PosicaoInvalidaException {
+	public void construtor_deveLancarPosicaoInvalidaException_seColunaPassadaForMaiorQueOPermitido() throws PosicaoInvalidaException {
 		assertThrows(PosicaoInvalidaException.class, () -> {
 			new Posicao(POSICAO_VALIDA, POSICAO_ACIMA_DO_MAXIMO);
 		});
 	}
 	
 	@Test
-	@DisplayName("Deve construir Posicao com sucesso se linha e coluna forem corretos")
-	public void construirComSucesso() throws PosicaoInvalidaException {
+	public void construtor_deveSerInstanciadoComSucesso_seLinhaEColunaPassadosForemValido() throws PosicaoInvalidaException {
 		assertDoesNotThrow(() -> {
 			new Posicao(POSICAO_VALIDA, POSICAO_VALIDA);
 		});
