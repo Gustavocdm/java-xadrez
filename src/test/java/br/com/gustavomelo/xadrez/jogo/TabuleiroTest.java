ac792b53f96beed1615bfc81af4d25616b566109
Gustavo <47989383+Gustavocdm@users.noreply.github.com>
2022-05-05 09:54:49 -0300

    test: adicionar peça ao tabuleiro

--- src/test/java/br/com/gustavomelo/xadrez/jogo/TabuleiroTest.java
+++ src/test/java/br/com/gustavomelo/xadrez/jogo/TabuleiroTest.java
@@ -1,7 +1,38 @@
 package br.com.gustavomelo.xadrez.jogo;
 
-import junit.framework.TestCase;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class TabuleiroTest extends TestCase {
+import org.junit.jupiter.api.Test;
 
+import br.com.gustavomelo.xadrez.exceptions.CasaPossuiPecaException;
+import br.com.gustavomelo.xadrez.factories.PeaoFactory;
+import br.com.gustavomelo.xadrez.models.Peca;
+import br.com.gustavomelo.xadrez.utils.Posicao;
+
+public class TabuleiroTest {
+	private Tabuleiro tabuleiro = new Tabuleiro();
+	
+	@Test
+	public void adicionarPecaAoTabuleiro_deveLancarCasaPossuiPecaException_seJaExistiPecaNaCasa() {
+		Peca peaoBranco = new PeaoFactory().criarPecaBrancaNaPosicao(new Posicao(1, 1));
+		
+		tabuleiro.adicionarPecaAoTabuleiro(peaoBranco);
+		
+		Peca peaoPreto = new PeaoFactory().criarPecaPretaNaPosicao(new Posicao(1, 1));
+		
+		assertThrows(CasaPossuiPecaException.class, () -> {
+			tabuleiro.adicionarPecaAoTabuleiro(peaoPreto);
+		});
+	}
+	
+	@Test
+	public void adicionarPecaAoTabuleiro_deveAdicionarPecaAoTabuleiro_sePosicaoValida() {
+		Peca peaoBranco = new PeaoFactory().criarPecaBrancaNaPosicao(new Posicao(1, 1));
+		
+		tabuleiro.adicionarPecaAoTabuleiro(peaoBranco);
+		Peca pecaDaPosicaoEspecificada = tabuleiro.getTabuleiro()[0][0];
+				
+		assertEquals(peaoBranco, pecaDaPosicaoEspecificada);
+	}
 }
